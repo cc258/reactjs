02f3a3b43ff1b0341ca778f74ae41d03eb022c7f
Leo <chuang.cui@ericsson.com>
2019-06-19 16:51:58 +0800

    feat: leaflet map

--- static/src/pages/simulator/simulator.jsx
+++ static/src/pages/simulator/simulator.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { formatMessage, injectIntl } from "react-intl";
+import SimulatorMap from "./map.jsx";
 
 class Simulator extends React.Component {
   render() {
@@ -7,14 +7,24 @@ class Simulator extends React.Component {
       <div className="pages home simulator">
         <section className="simulator-cont">
           <div className="simulator-card">
-            <div class="simulator-car" />
+            <div className="simulator-car" />
           </div>
-          <div className="simulator-card">map</div>
-          <div className="simulator-card">log</div>
+          <div className="simulator-card">
+            <SimulatorMap />
+          </div>
+          <div className="simulator-card">
+            <ul className="simulator-log">
+              <li>向南行驶</li>
+              <li>靠左，并入科韵路</li>
+              <li>稍微向右转，进入琶洲大道东</li>
+              <li>向右转</li>
+              <li>广州琶洲会展中心</li>
+            </ul>
+          </div>
         </section>
       </div>
     );
   }
 }
 
-export default injectIntl(Simulator);
+export default Simulator;
