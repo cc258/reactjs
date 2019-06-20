ee899327f2afd9073298bc889a2cbdfd025f3709
cc home <cc258520@gmail.com>
2019-06-21 02:11:03 +0800

    perf: react

--- static/src/pages/websocket/websocket.jsx
+++ static/src/pages/websocket/websocket.jsx
@@ -3,27 +3,29 @@ import React, { useState, useEffect } from "react";
 export default function Websocket() {
   const [msg, setMsg] = useState("");
   const [txt, setTxt] = useState("");
-
   const socket = new WebSocket("ws://localhost:7080");
-  console.log("【client】", socket);
-  socket.onerror = function() {
-    console.log("【client】websocket error");
-  };
-  socket.onopen = function() {
-    console.log("websocket connect success");
-    // socket.send("【client】hello websocket");
-  };
-  socket.onmessage = function(event) {
-    console.log("【client】websocket data:", event);
-    setMsg(event.data);
-  };
-  socket.onclose = function() {
-    console.log("【client】websocket close");
-  };
 
-  socket.onbeforeunload = function() {
-    websocket.close();
-  };
+  useEffect(() => {
+    console.log("【client】", socket);
+    socket.onerror = function() {
+      console.log("【client】websocket error");
+    };
+    socket.onopen = function() {
+      console.log("websocket connect success");
+      // socket.send("【client】hello websocket");
+    };
+    socket.onmessage = function(event) {
+      console.log("【client】websocket data:", event);
+      setMsg(event.data);
+    };
+    socket.onclose = function() {
+      console.log("【client】websocket close");
+    };
+
+    socket.onbeforeunload = function() {
+      websocket.close();
+    };
+  }, []);
 
   function handChangeTxt(e) {
     setTxt(e.target.value);
