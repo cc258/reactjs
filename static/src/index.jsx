fc360d760965fe10b2986c786266825cb866d712
Leo <chuang.cui@ericsson.com>
2019-07-19 17:57:26 +0800

    webpack

--- static/src/index.jsx
+++ static/src/index.jsx
@@ -1,41 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, combineReducers } from "redux";
-
-// international language
-import { addLocaleData, IntlProvider } from "react-intl";
-import en from "react-intl/locale-data/en";
-import zh from "react-intl/locale-data/zh";
-import zh_CN from "../lang/zh_CN";
-import en_US from "../lang/en_US";
-
-import thunk from "redux-thunk";
-import todos from "./pages/todo/todos.reducer";
-
-import Routers from "./pages/routers/routers.jsx";
-
-import "./css/app.css";
-
-addLocaleData([...en, ...zh]);
-
-const INITIAL = {};
-
-const store = createStore(
-  combineReducers({
-    INITIAL,
-    todos
-  }),
-  applyMiddleware(thunk)
-);
+import { hot } from "react-hot-loader";
+import App from "./app.jsx";
+const HotApp = hot(module)(App);
 
 const mount = document.getElementById("app");
-render(
-  // navigator.language
-  <Provider store={store}>
-    <IntlProvider locale={"en"} key={"en"} locale={"en"} messages={en_US}>
-      <Routers />
-    </IntlProvider>
-  </Provider>,
-  mount
-);
+render(<HotApp />, mount);
