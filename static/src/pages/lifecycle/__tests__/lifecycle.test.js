5f48771e40707d0d16cbb90d48c1f2710e4f90e8
cc home <cc258520@gmail.com>
2020-04-30 01:50:01 +0800

    feat: igore coverage

--- static/src/pages/lifecycle/__tests__/lifecycle.test.js
+++ static/src/pages/lifecycle/__tests__/lifecycle.test.js
@@ -2,23 +2,27 @@ import React from 'react'
 import Lifecycle from '../lifecycle'
 import { shallow } from 'enzyme';
 
-// import axios from 'axios'
-// import MockAdapter from "axios-mock-adapter"
+import axios from 'axios'
+import MockAdapter from "axios-mock-adapter"
 
 describe('<Lifecycle />', () => {
 	beforeEach(function () {
-		// const mock = new MockAdapter(axios);
-		// mock.onGet("https://api.github.com/users").reply(200, {
-		// 	users: [{ login: "John Smith" }],
-		// });
+		const mock = new MockAdapter(axios);
+		mock.onGet("https://api.github.com/users").reply(200, {
+			users: [{ login: "John Smith" }],
+		});
+
+		mock.onGet("/users/12345").reply(200, {
+			users: [{ login: "John Smith" }],
+		});
 	})
 
 	it('render scorret', () => {
 		const wrapper = shallow(<Lifecycle />)
+		const ins = wrapper.instance()
 		expect(wrapper).toMatchSnapshot();
-		// axios.get("https://api.github.com/users").then(function (response) {
-		// 	console.log(response.data);
-		// });
+
+		ins.getApi();
 
 	})
 
