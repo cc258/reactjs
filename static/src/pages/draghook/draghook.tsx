ce875090d6cfd9e3d0ef982aa99c1d60fc183eec
Chuang Cui <chuang.cui@ericsson.com>
2021-09-08 11:16:09 +0800

    feat: draghook

--- static/src/pages/draghook/draghook.tsx
+++ static/src/pages/draghook/draghook.tsx
@@ -7,34 +7,41 @@ import './draghook.scss';
 const DragHook: FC = (props: any): ReactElement => {
   const [container, setContainer] = useState<HTMLDivElement>();
 
-  const handleDrop = useCallback(
-    (el, target, source) => {
-      const sourceId = source.classList.contains('card-body');
-      const targetId = target.classList.contains('card-body');
-      // onChange({
-      //   source: { listId: sourceId, itemIds: 1 },
-      //   target: { listId: targetId, itemIds: 2 }
-      // });
-    },
-    // [onChange]
-    []
-  );
+  // const handleDrop = useCallback(
+  //   (el, target, source) => {
+  //     const sourceId = source.classList.contains('card-body');
+  //     const targetId = target.classList.contains('card-body');
+  //     // onChange({
+  //     //   source: { listId: sourceId, itemIds: 1 },
+  //     //   target: { listId: targetId, itemIds: 2 }
+  //     // });
+  //   },
+  //   // [onChange]
+  //   []
+  // );
 
+  const handleDrag = useCallback(() =>{
+    console.log('Drag');
+  }, [container])
+  const handleDrop = useCallback(() =>{
+    console.log('Drop');
+  }, [container])
 
 
   useEffect(() => {
     if(!container){
       return
     }
-    const cards:any = Array.from(container.querySelectorAll('.card'));
+    const cards:any = Array.from(container.querySelectorAll('.container'));
     console.log(cards);
     const drake = dragula(cards , {
       moves(el, source, handle) {
-        console.log(el);
-        return source.classList.contains('.card');
+        console.log('el', el);
+        return source.classList.contains('container');
       }
     });
-    // drake.on("drop", handleDrop);
+    drake.on("drag", handleDrag);
+    drake.on("drop", handleDrop);
 
     return () => {
       drake.destroy()
@@ -62,10 +69,10 @@ const DragHook: FC = (props: any): ReactElement => {
 const Card: FC = (props: any): ReactElement => {
   return (
     <div className="card">
-      <div className="card-header">
+      <div className="header">
         <h3>Example Card Header</h3>
       </div>
-      <div className="card-body">
+      <div className="body">
         <p>{props.text}</p>
       </div>
     </div>
