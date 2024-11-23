faebf8b3ee12ec7c1c16da2b15207266bbec2ef7
chuang.c <chuang.c@pg.com>
2024-11-23 23:05:33 +0800

    feat: antd table

--- static/src/pages/chrome/chrome.jsx
+++ static/src/pages/chrome/chrome.jsx
@@ -1,15 +1,11 @@
 import React, { memo, useState, useEffect, useCallback, useContext, useReducer, useRef } from 'react';
-import { Row, Col, Button, Checkbox, Form, Input, Switch } from 'antd';
+import { Row, Col, Button, Checkbox, Form, Input, Switch,Table, InputNumber, Popconfirm } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
 
 /* Chrome调试技巧 */
 const Chrome = () => {
   const [obj, setObj] = useState({});
   // const [edit, setEdit] = useState(false);
-
-  const [disableForm, setDisableForm] = useState(false);
-
-  const [form] = Form.useForm();
 
   const Kaselin = 18;
 
@@ -27,77 +23,203 @@ const Chrome = () => {
   console.table(obj.like);
   console.table(obj.like && obj.like.ball);
 
-  const onFinish = v => {
-    console.log(`onFinish: `, v);
-  };
-
-  const onFinishFailed = e => {
-    console.log(`onFinishFailed: `, e);
-  };
-
-  const onReset = () => {
-    form.resetFields();
-  };
-
-  const onFill = () => {
-    form.setFieldsValue({ username: 'cc', password: '123' });
-  };
 
   return (
     <section className="pages">
-      <div>create plan</div>
-
-      <Row>
-        <Col span={8}></Col>
-        <Col span={10}>
-          <label>
-            Disable Form
-            <Switch onChange={setDisableForm} />
-          </label>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={24}>
-          <Form
-            form={form}
-            name="basic"
-            labelCol={{ md: { span: 8 } }}
-            wrapperCol={{ md: { span: 10 } }}
-            initialValues={{ member: true }}
-            disabled={disableForm}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-          >
-            <Form.Item
-              label="name"
-              name="username"
-              rules={[{ required: true, message: 'Please input your username!' }]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item label="password" name="password" rules={[{ required: true, message: 'Please input your pwd!' }]}>
-              <Input.Password />
-            </Form.Item>
-            <Form.Item name="member" valuePropName="checked" wrapperCol={{ offset: 8, span: 10 }}>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-            <Form.Item wrapperCol={{ offset: 8, span: 10 }}>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-              <Button htmlType="button" onClick={onReset}>
-                Reset
-              </Button>
-              <Button htmlType="button" onClick={onFill}>
-                Fill
-              </Button>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
+      <EditableTable />
     </section>
   );
 };
+
+
+const data = [];
+for (let i = 0; i < 100; i++) {
+  data.push({
+    key: i.toString(),
+    name: `Edrward ${i}`,
+    age: 32,
+    address: `London Park no. ${i}`,
+  });
+}
+
+const EditableContext = React.createContext();
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+);
+const EditableFormRow = Form.create()(EditableRow);
+class EditableCell extends React.Component {
+  getInput = () => {
+    if (this.props.inputType === 'number') {
+      return <InputNumber />;
+    }
+    return <Input />;
+  };
+
+  renderCell = ({ getFieldDecorator }) => {
+    const {
+      editing,
+      dataIndex,
+      title,
+      inputType,
+      record,
+      index,
+      children,
+      ...restProps
+    } = this.props;
+    return (
+      <td {...restProps}>
+        {editing ? (
+          <Form.Item style={{ margin: 0 }}>
+            {getFieldDecorator(dataIndex, {
+              rules: [
+                {
+                  required: true,
+                  message: `Please Input ${title}!`,
+                },
+              ],
+              initialValue: record[dataIndex],
+            })(this.getInput())}
+          </Form.Item>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  };
+
+  render() {
+    return <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer>;
+  }
+}
+
+class EditableTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data, editingKey: '' };
+    this.columns = [
+      {
+        title: 'name',
+        dataIndex: 'name',
+        width: '25%',
+        editable: true,
+      },
+      {
+        title: 'age',
+        dataIndex: 'age',
+        width: '15%',
+        editable: true,
+      },
+      {
+        title: 'address',
+        dataIndex: 'address',
+        width: '40%',
+        editable: true,
+      },
+      {
+        title: 'operation',
+        dataIndex: 'operation',
+        render: (text, record) => {
+          const { editingKey } = this.state;
+          const editable = this.isEditing(record);
+          return editable ? (
+            <span>
+              <EditableContext.Consumer>
+                {form => (
+                  <a
+                    onClick={() => this.save(form, record.key)}
+                    style={{ marginRight: 8 }}
+                  >
+                    Save
+                  </a>
+                )}
+              </EditableContext.Consumer>
+              <Popconfirm title="Sure to cancel?" onConfirm={() => this.cancel(record.key)}>
+                <a>Cancel</a>
+              </Popconfirm>
+            </span>
+          ) : (
+            <a disabled={editingKey !== ''} onClick={() => this.edit(record.key)}>
+              Edit
+            </a>
+          );
+        },
+      },
+    ];
+  }
+
+  isEditing = record => record.key === this.state.editingKey;
+
+  cancel = () => {
+    this.setState({ editingKey: '' });
+  };
+
+  save(form, key) {
+    form.validateFields((error, row) => {
+      if (error) {
+        return;
+      }
+      const newData = [...this.state.data];
+      const index = newData.findIndex(item => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        this.setState({ data: newData, editingKey: '' });
+      } else {
+        newData.push(row);
+        this.setState({ data: newData, editingKey: '' });
+      }
+    });
+  }
+
+  edit(key) {
+    console.log('click key: ', key)
+    this.setState({ editingKey: key });
+  }
+
+  render() {
+    const components = {
+      body: {
+        row: EditableFormRow,
+        cell: EditableCell,
+      },
+    };
+
+    const columns = this.columns.map(col => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          inputType: col.dataIndex === 'age' ? 'number' : 'text',
+          dataIndex: col.dataIndex,
+          title: col.title,
+          editing: this.isEditing(record),
+        }),
+      };
+    });
+
+    return (
+        <Table
+          components={components}
+          bordered
+          dataSource={this.state.data}
+          columns={columns}
+          rowClassName="editable-row"
+          pagination={{
+            onChange: this.cancel,
+          }}
+        />
+    );
+  }
+}
+
 
 const ChromeMemo = memo(Chrome);
 
